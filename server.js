369d3fff5fd2708025f389dacbb39d8da95c424c
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-02-18 10:17:57 +0530

    server pushed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: 'https://corona-chat.vercel.app', // Replace with your frontend URL
     methods: ['GET', 'POST'],
   },
 });
