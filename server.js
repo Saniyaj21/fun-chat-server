3effa17576873ce52a45d391933a455a4a6d2b92
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-02-19 09:21:20 +0530

    patch update

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ io.on('connection', (socket) => {
 });
 
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 server.listen(3001, () => {
