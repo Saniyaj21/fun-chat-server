493397140b5338286dbda35912781c7d9787d4f5
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-02-19 09:54:15 +0530

    patch update

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const server = http.createServer(app);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// CORS configuration for Express
+app.use(cors({
+  origin: "*",
+  exposedHeaders: ['X-Total-Count'],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "X-Total-Count"],
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+},
+
+
+));
+
+// Socket.IO configuration
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+  transports: ['websocket', 'polling'], // Explicitly allow both
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  console.log('A user connected');
+
+  socket.on('message', (message) => {
+    io.emit('message', message); // Broadcast the message to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 server.listen(8080, () => {
-  console.log('server running at http://localhost:8080');
+  console.log('Server is running on port 3001');
 });
